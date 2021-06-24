5a2a4af59f658aa8b5763688e5bcb98c6e623f54
malvdev <sergey.malkov.dev@gmail.com>
2021-06-24 11:28:02 +0500

    refactor: shorthand bubble sort

--- src/bubble-sort/bubble-sort.js
+++ src/bubble-sort/bubble-sort.js
@@ -1,11 +1,8 @@
 export default function bubbleSort(array) {
   for (let i = 0; i < array.length; i += 1) {
     for (let j = 0; j < array.length - i - 1; j += 1) {
-      const compareFirst = array[j];
-      const compareSecond = array[j + 1];
-      if (compareSecond < compareFirst) {
-        array[j] = compareSecond;
-        array[j + 1] = compareFirst;
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
       }
     }
   }
